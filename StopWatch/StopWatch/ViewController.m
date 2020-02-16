5e8741439619b70af6863aa1f31bfa7316721db0
Vici Shaweddy <vshaweddy@gmail.com>
2020-02-15 20:28:31 -0500

    added the observeValueForKeyPath

--- StopWatch/StopWatch/ViewController.m
+++ StopWatch/StopWatch/ViewController.m
@@ -10,7 +10,9 @@
 #import "LSIStopWatch.h"
 
 
-// TODO: Create a KVOContext to identify the StopWatch observer
+// Create a KVOContext to identify the StopWatch observer
+// void * = id = AnyObject (pointer to any class)
+void *KVOContext = &KVOContext; // 0x123 == 291 // just a number
 
 // KVO = Key Value Observing
 // Listen for changes on a property
@@ -81,7 +83,9 @@
         _stopwatch = stopwatch;
         
         // didSet
-		// TODO: Setup KVO - Add Observers
+		// Setup KVO - Add Observers
+        [_stopwatch addObserver:self forKeyPath:@"running" options:0 context:KVOContext];
+        [_stopwatch addObserver:self forKeyPath:@"elapsedTime" options:0 context:KVOContext];
     }
     
 }
@@ -89,6 +93,22 @@
 
 // TODO: Review docs and implement observerValueForKeyPath
 
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    if (context == KVOContext) {
+        if ([keyPath isEqualToString:@"running"]) {
+            [self updateViews];
+            printf("Update the UI: %i\n", self.stopwatch.running);
+        } else if ([keyPath isEqualToString:@"elapsedTime"]){
+            [self updateViews];
+            printf("Elapsed time: %.2f\n", self.stopwatch.elapsedTime);
+        }
+        
+    } else {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+}
 
 - (void)dealloc {
 	// TODO: Stop observing KVO (otherwise it will crash randomly)
